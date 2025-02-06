5ed9d8fb63433af74e96260997553b0cbd041866
Brian Fohl <brian.fohl@gmail.com>
2025-02-06 12:59:56 -0800

    formatting fix for mobile

--- app/components/PortfolioSite.tsx
+++ app/components/PortfolioSite.tsx
@@ -139,10 +139,10 @@ const PortfolioSite = () => {
       >
         <div className="absolute inset-0 bg-gradient-to-b from-black via-transparent to-black opacity-75"></div>
         <div className="relative text-center p-8 rounded mt-[10%]">
-          <h1 className="text-9xl font-bold mb-4 text-white">
+          <h1 className="text-5xl md:text-7xl lg:text-9xl font-bold mb-4 text-white">
             BRIAN FOHL
           </h1>
-          <p className="mt-4 max-w-2xl mx-auto text-gray-300 text-lg text-white">
+          <p className="mt-4 max-w-2xl mx-auto text-gray-300 text-base md:text-lg text-white">
             Engineering Leader & DevOps Architect
           </p>
         </div>
